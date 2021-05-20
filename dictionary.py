2ca7b4ca4ec05b4d2f6015663e07dbd75f3cda96
Thomas Hunkin <tjhunkin@primesyntax.com>
2021-05-20 20:00:29 +0200

    dictionaries

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,20 @@
 # Dictionary items are ordered, changeable, and does not allow duplicates.
 # As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
 
+# methods
+
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
+
 # create
 
 thisdict = {
@@ -28,3 +42,217 @@ thisdict = {
 }
 print(thisdict)
 
+# length
+print(len(thisdict))
+
+# get item
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+# or
+
+x = thisdict.get("model")
+print(x)
+
+# get list of keys
+x = thisdict.keys()
+print(x)
+
+# add new item to dictionary
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x)  # before the change
+
+car["color"] = "white"
+
+print(x)  # after the change
+
+# get values
+x = thisdict.values()
+print(x)
+
+
+x = thisdict.items()
+print(x)
+
+for y in x:
+    print(y)
+
+# key exists
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+# change values
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+# or
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+print(thisdict)
+
+# remove
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+# remove last item
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+# or
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+# delete dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+# print(thisdict) this will cause an error because "thisdict" no longer exists.
+
+# empty
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# loop keys
+for x in thisdict:
+    print(x)
+
+# or
+
+for x in thisdict.keys():
+    print(x)
+
+# loop values
+for x in thisdict:
+    print(thisdict[x])
+
+# or
+
+for x in thisdict.values():
+    print(x)
+
+# loop keys and values
+
+for x, y in thisdict.items():
+    print(x, y)
+
+# copy
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# or
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+# nested
+
+myfamily = {
+  "child1": {
+    "name": "Emil",
+    "year": 2004
+  },
+  "child2": {
+    "name": "Tobias",
+    "year": 2007
+  },
+  "child3": {
+    "name": "Linus",
+    "year": 2011
+  }
+}
+
+print(myfamily)
+
+# or
+
+child1 = {
+  "name": "Emil",
+  "year": 2004
+}
+child2 = {
+  "name": "Tobias",
+  "year": 2007
+}
+child3 = {
+  "name": "Linus",
+  "year": 2011
+}
+
+myfamily = {
+  "child1": child1,
+  "child2": child2,
+  "child3": child3
+}
+
+print(myfamily)
