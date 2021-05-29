0e2e7ddbedcd0b0fa96b698c4ecb1979ad2f7f62
Thomas Hunkin <tjhunkin@primesyntax.com>
2021-05-29 14:23:13 +0200

    moved graphs to their own folder started machibe learning

---
+++ machineLearning/median.py
@@ -0,0 +1,7 @@
+import numpy
+
+speed = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+
+x = numpy.median(speed)  # The median value is the value in the middle, after you have sorted all the values:
+
+print(x)
